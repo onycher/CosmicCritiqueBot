3a7a6eef004f1f3ece06e90aa6710d6320dd0258
Sebastian Onycher <onycher@gmail.com>
2024-10-26 00:53:14 +0200

    Set proper site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://onycher.github.io/CosmicCritiqueBot',
 	integrations: [mdx(), sitemap()],
 });
